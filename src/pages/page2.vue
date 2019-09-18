c94fb9d88ede3d0805a0530a6415fe495a0559ae
baipengfei <baipf@yonyou.com>
2019-09-18 22:56:39 +0800

    上传图片

--- src/pages/page2.vue
+++ src/pages/page2.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="page2 my-worker">
-    <p style="font-size: 14px;margin-left: 16px">共计作业：2</p>
+    <p style="font-size: 14px;margin-left: 16px">共计作业：{{workerList.length}}</p>
     <i-cell-group>
       <i-cell v-for="(item,index) in workerList" :key="index" :title="item.title" @click="workerDetail(item)">
         <span slot="icon">
           <!--<i-icon :type="item.icon" size="20" color="#80848f" />-->
-          <!--<img :src="item.image" style="width:100px;height:50px;background-size: 100% 100%"/>-->
           <div style="width:120px;height:40px;margin-right: 10px">
             <img :src="item.image" style="max-width: 100%;max-height: 100%"/>
           </div>
-
         </span>
       </i-cell>
     </i-cell-group>
@@ -22,8 +20,20 @@ import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class Page2 extends Vue {
   private workerList:Array<any> = [
-    {image:require('../../static/images/20.png'), title:'Year 1 CAD Assignment 1'},
-    {image:require('../../static/images/20.png'), title:'Year 1 Sketchup Assignment 1'}
+    {image:require('../../static/images/1.png'), title:'Year 1 CAD Assignment 1'},
+    {image:require('../../static/images/2.png'), title:'Year 1 Sketchup Assignment 1'},
+    {image:require('../../static/images/3.png'), title:'Year 1 CAD Assignment 1'},
+    {image:require('../../static/images/4.png'), title:'Year 1 Sketchup Assignment 1'},
+    {image:require('../../static/images/5.png'), title:'Year 1 CAD Assignment 1'},
+    {image:require('../../static/images/6.png'), title:'Year 1 Sketchup Assignment 1'},
+    {image:require('../../static/images/7.png'), title:'Year 1 CAD Assignment 1'},
+    {image:require('../../static/images/8.png'), title:'Year 1 Sketchup Assignment 1'},
+    {image:require('../../static/images/9.png'), title:'Year 1 CAD Assignment 1'},
+    {image:require('../../static/images/10.png'), title:'Year 1 Sketchup Assignment 1'},
+    {image:require('../../static/images/11.png'), title:'Year 1 CAD Assignment 1'},
+    {image:require('../../static/images/12.png'), title:'Year 1 Sketchup Assignment 1'},
+    {image:require('../../static/images/13.png'), title:'Year 1 CAD Assignment 1'},
+    {image:require('../../static/images/14.png'), title:'Year 1 Sketchup Assignment 1'},
   ]
 
   workerDetail(item){
