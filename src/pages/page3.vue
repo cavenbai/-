d75989423aa26a124d38e352fb5468ba47226597
baipengfei02 <baipengfei02@megvii.com>
2019-09-29 13:58:54 +0800

    fix:解决步骤条样式问题

--- src/pages/page3.vue
+++ src/pages/page3.vue
@@ -2,20 +2,23 @@
   <div class="page3 worker-detail">
     <h1 style="text-align:center;padding-top:30px;">{{title}}</h1>
     <div style="margin-top: 20px;">
-      <div style="position:relative;display: flex;font-size: 13px;height:110px;" v-for="(item,index) in detail" :key="index">
-        <p style="margin-top:16px;margin-right:10px;margin-left: 19%;">{{item.timestamp}}</p>
-        <div style="width: 20%;display: flex;justify-content: center;">
-          <div v-if="item.percentage === 50 || item.percentage === 100" style="width:50px;height:50px;border-radius:50%;background:aqua;display:flex;justify-content:center;align-items:center;font-size:16px;">
+      <div style="display:flex;font-size: 13px;height:110px;justify-content: center" v-for="(item,index) in detail" :key="index">
+        <div v-if="item.percentage === 50 || item.percentage === 100" style="width: 80px;text-align:right;margin-top: 10px">{{item.timestamp}}</div>
+        <div v-else style="width: 80px;text-align:right">{{item.timestamp}}</div>
+        <div v-if="item.percentage === 50 || item.percentage === 100" style="width: 70px;font-size:16px;position: relative">
+          <div style="width:50px;height:50px;border-radius:50%;background:aqua;display: flex;justify-content: center;align-items: center;margin: 0 auto;">
             {{item.percentage}}%
           </div>
-          <div v-else style="width:25px;height:25px;border-radius:50%;background:#d8d2d4;display:flex;justify-content:center;align-items:center;font-size:10px;">
+          <div style="width:1px;height:60px;background:black;position:absolute;top:45%;left:34px;"></div>
+        </div>
+        <div v-else style="width: 70px;font-size:10px;position: relative">
+          <div style="width:25px;height:25px;border-radius:50%;background:#d8d2d4;display: flex;justify-content: center;align-items: center;margin: 0 auto;">
             {{item.percentage}}%
           </div>
+          <div style="width:1px;height:85px;background:black;position:absolute;top:23%;left:34px;"></div>
         </div>
-        <div style="margin-top: 16px;margin-left: 10px">
-          <p>{{item.description}}</p>
-        </div>
-        <div style="width:1px;height:60px;background:black;position:absolute;top:45%;left:46%;"></div>
+        <div v-if="item.percentage === 50 || item.percentage === 100" style="width: 150px;margin-top: 10px">{{item.description}}</div>
+        <div v-else style="width: 150px;">{{item.description}}</div>
       </div>
     </div>
   </div>
